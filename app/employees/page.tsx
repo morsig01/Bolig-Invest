969bc0992f0478ace27b84d245cc6f3977b66d34
Morgan Sigland <morgan.sigland@gmail.com>
2025-01-14 01:42:32 +0100

    fix shadcn

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -21,7 +21,7 @@ export default async function EmployeesPage() {
 
   return (
     <div className="container mx-auto p-4 py-40">
-      <h1 className="text-3xl font-bold mb-6">Our Team</h1>
+      <div className="text-3xl font-bold mb-6">Our Team</div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.map((employee: any) => (
           <EmployeeCard
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import EmployeeSlider from "@/components/EmployeeSlider";
 import Hero from "@/components/Hero";
 import ProjectsLink from "@/components/ProjectsLink";
 import Services from "@/components/Services";
+import VideoPlayer from "@/components/VideoPlayer";
 
 export default function Home() {
   return (
-  <div>
-    <Hero />
-    <About />
-    <ProjectsLink />
-    <Services />
-    <EmployeeSlider />
-  </div>
+    <div>
+      <Hero />
+      <VideoPlayer         
+        src="/sample-video.mp4"
+      />
+      <About />
+      <ProjectsLink />
+      <Services />
+      <EmployeeSlider />
+    </div>
   );
 }
